391f94fbdc271f4003adfb25fbfdc2e3a86bca36
marceloarmando <mcuellarr98@gmail.com>
2020-07-09 11:25:37 -0500

    commit 3

--- database.js
+++ database.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'userdemo';
-const MONGO_PASSWORD = 'Tecsup';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'contacts';
-
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
-mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/contacts', {
+    useNewUrlParser: true
+})
+    .then(db => console.log('Db is connected'))
+    .catch(err => console.log(err));
--- src/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/contacts', {
-    useNewUrlParser: true
-})
-    .then(db => console.log('Db is connected'))
-    .catch(err => console.log(err));
